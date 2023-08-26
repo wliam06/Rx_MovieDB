a91c9c073c056487c6d29f2013a217c3211afcbf
william-gho <william@ajaib.co.id>
2023-08-26 14:43:38 +0700

    fix: movie detail sample info.plist

--- MovieDetail/MovieDetailSample/AppDelegate.swift
+++ MovieDetail/MovieDetailSample/AppDelegate.swift
@@ -10,10 +10,15 @@ import UIKit
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
+    var window: UIWindow?
+    var coordinator: MovieDetailSampleCoordinator!
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        self.window = UIWindow(frame: UIScreen.main.bounds)
+        coordinator = MovieDetailSampleCoordinator(window: window)
+        coordinator.start()
+        self.window?.makeKeyAndVisible()
         return true
     }
 
--- MovieDetail/MovieDetailSample/ViewController.swift
+++ MovieDetail/MovieDetailSample/ViewController.swift
@@ -9,11 +9,31 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
+        
+        super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        self.view.backgroundColor = .red
         // Do any additional setup after loading the view.
     }
+    
 
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
 
 }
-
